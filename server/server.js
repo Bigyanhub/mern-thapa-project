3e328e7cee557c9d94126237f46f85c9163e433e
Bigyanhub <bigyanbasnet849@gmail.com>
2025-08-15 17:36:35 +0545

    Removes bundled node modules and lock file
    Deletes all files under the node_modules directory, including third-party dependencies and the package lock file, to eliminate locally bundled modules.
    
    This streamlines the repository and prepares for a clean dependency installation via package manager, reducing clutter and potential version conflicts.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router")
+const connectDb = require("./utils/db")
 
 //Middleware that parse JSON from request(incoming to backend)
 app.use(express.json());
@@ -8,8 +10,10 @@ app.use(express.json());
 /* --------------------------- Sends to the router -------------------------- */
 app.use("/api/auth", router);
 
-
+connectDb().then(()=>{
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
+})
+
